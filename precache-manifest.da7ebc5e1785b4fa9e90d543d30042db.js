3fb93608add8ab549b391f8d3c5953c139c955cd
Davionli <davionli1007@gmail.com>
2019-08-02 10:07:29 -0700

    Updates

---
+++ precache-manifest.da7ebc5e1785b4fa9e90d543d30042db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64d9ee274e8373fab42fe28c4f79e7b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Clicky-Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7da38470c833b2a7e798",
+    "url": "/Clicky-Game/static/js/main.8baa57b8.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
